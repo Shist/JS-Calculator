89439f375641b0ff31d693b27236671899135ec4
Shist <shist.pupust@mail.ru>
2024-03-06 08:28:32 +0300

    Added default ESLint settings

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["warn", 2],
+    "linebreak-style": ["warn", "windows"],
+    quotes: ["warn", "double"],
+    semi: ["warn", "always"],
+    "no-unused-vars": ["warn"],
+    "no-undef": ["warn"],
+  },
+};
